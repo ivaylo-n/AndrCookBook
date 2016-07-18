35c8e7ab5af3bd0b121567e64650989dd0b1d29e
Ivailo N <ivoned.st@gmail.com>
2016-07-18 13:30:01 +0300

    add Views Widgets Styles

---
+++ Views Widgets Styles/RuntimeWidget/app/src/main/java/com/example/koki/runtimewidget/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.koki.runtimewidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.layout);
+        DatePicker datePicker = new DatePicker(this);
+        layout.addView(datePicker);
+    }
+}
