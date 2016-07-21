444782b96285a1f449a5ce5024c24bb2069d6094
Ivailo N <ivoned.st@gmail.com>
2016-07-22 02:48:10 +0300

    Add Fragments-AppWidgets-SystemUI

---
+++ Fragments-AppWidgets-SystemUI/FragmentCommunication/app/src/main/java/com/example/koki/fragmentcommunication/MasterFragment.java
@@ -0,0 +1,41 @@
+package com.example.koki.fragmentcommunication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MasterFragment extends ListFragment {
+    public interface OnMasterSelectedListener {
+        public void onItemSelected(String countryName);
+    }
+    private OnMasterSelectedListener mOnMasterSelectedListener = null;
+    public  void setOnMasterSelectedListener(OnMasterSelectedListener listener) {
+        mOnMasterSelectedListener = listener;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String[] countries = new String[]{"China","France",
+        "Germany","India","Singapore","UK","US"};
+        ListAdapter countryAdapter = new ArrayAdapter<String>(
+                getActivity(),android.R.layout.simple_list_item_1,countries);
+        setListAdapter(countryAdapter);
+        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (mOnMasterSelectedListener != null) {
+                    mOnMasterSelectedListener.onItemSelected(((
+                            TextView) view).getText().toString());
+                }
+            }
+        });
+    }
+}
