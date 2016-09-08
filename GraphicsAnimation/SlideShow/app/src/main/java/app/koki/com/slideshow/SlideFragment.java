53ed8f83ec0a25f01e2ad04bfe939d35cef4bc92
Ivailo N <ivoned.st@gmail.com>
2016-09-08 15:55:36 +0300

    Add Graphics and Animation

---
+++ GraphicsAnimation/SlideShow/app/src/main/java/app/koki/com/slideshow/SlideFragment.java
@@ -0,0 +1,30 @@
+package app.koki.com.slideshow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+public class SlideFragment extends Fragment {
+    private int mImageResourceID;
+    public SlideFragment(){}
+    public void setImage(int resourceID){
+        mImageResourceID = resourceID;
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,  ViewGroup container,
+            Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_slide, container, false);
+        ImageView imageView = (ImageView)rootView.findViewById(
+                R.id.imageView);
+        imageView.setImageResource(mImageResourceID);
+        return rootView;
+    }
+}
